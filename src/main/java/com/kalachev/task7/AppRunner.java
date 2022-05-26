5d18a8c772cb8a5bd45fc64c31e2cdbcd181e4ed
aoshaaa <kalachev.artemm@gmail.com>
2022-05-26 15:43:27 +0300

    test-> appRunner

--- src/main/java/com/kalachev/task7/AppRunner.java
+++ src/main/java/com/kalachev/task7/AppRunner.java
@@ -1,14 +1,12 @@
 package com.kalachev.task7;
 
-import java.io.IOException;
-
 import com.kalachev.task7.dao.DaoException;
 import com.kalachev.task7.dao.DatabaseCreator;
 import com.kalachev.task7.ui.ConsoleApp;
 
-public class test {
+public class AppRunner {
 
-  public static void main(String[] args) throws DaoException, IOException {
+  public static void main(String[] args) throws DaoException {
 
     DatabaseCreator db = new DatabaseCreator();
     db.createDatabase();
