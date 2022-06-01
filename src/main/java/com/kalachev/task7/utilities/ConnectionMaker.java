e7db0d3db603e08accd5eb6dfc0d6a725779905f
aoshaaa <kalachev.artemm@gmail.com>
2022-06-01 13:39:56 +0300

    connection from property file now

--- src/main/java/com/kalachev/task7/utilities/ConnectionMaker.java
+++ src/main/java/com/kalachev/task7/utilities/ConnectionMaker.java
@@ -1,29 +1,41 @@
 package com.kalachev.task7.utilities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 import com.kalachev.task7.exceptions.DaoException;
 
 public class ConnectionMaker {
 
-  private static final String URL = "jdbc:postgresql://localhost/";
-  private static final String USERNAME = "postgres";
-  private static final String PASSWORD = "2487";
-
-  private static final String URL_CREATED = "jdbc:postgresql://localhost/comkalachevtasksqljdbc";
-  private static final String USERNAME_CREATED = "kalachevartemsql";
-  private static final String PASSWORD_CREATED = "1234";
+  static String dbUrl;
+  static String dbUsername;
+  static String dbPassword;
 
   private ConnectionMaker() {
     super();
   }
 
   public static Connection getDbConnection() throws DaoException {
+    Properties properties = new Properties();
+    URL url = ClassLoader.getSystemResource("DbProperties");
+    if (url != null) {
+      try (InputStream is = url.openStream()) {
+        properties.load(is);
+        dbUrl = (String) properties.get("URL_ORIGINAL");
+        dbUsername = (String) properties.get("USERNAME_ORIGINAL");
+        dbPassword = (String) properties.get("PASSWORD_ORIGINAL");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
     Connection connection = null;
     try {
       DriverManager.registerDriver(new org.postgresql.Driver());
-      connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+      connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
       return connection;
     } catch (Exception e) {
       e.printStackTrace();
@@ -32,11 +44,23 @@ public class ConnectionMaker {
   }
 
   public static Connection getDbConnectionForNewUser() throws DaoException {
+    Properties properties = new Properties();
+    URL url = ClassLoader.getSystemResource("DbProperties");
+    if (url != null) {
+      try (InputStream is = url.openStream()) {
+        properties.load(is);
+        dbUrl = (String) properties.get("URL_CREATED");
+        dbUsername = (String) properties.get("USERNAME_CREATED");
+        dbPassword = (String) properties.get("PASSWORD_CREATED");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
     Connection connection = null;
     try {
       DriverManager.registerDriver(new org.postgresql.Driver());
-      connection = DriverManager.getConnection(URL_CREATED, USERNAME_CREATED,
-          PASSWORD_CREATED);
+      connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
       return connection;
     } catch (Exception e) {
       e.printStackTrace();
