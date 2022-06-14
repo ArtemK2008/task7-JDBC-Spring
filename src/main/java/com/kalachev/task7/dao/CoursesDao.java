964029c4f9b7b97fe2c1d6939eb5f91670c2a972
aoshaaa <kalachev.artemm@gmail.com>
2022-06-14 15:54:24 +0300

    minor

--- src/main/java/com/kalachev/task7/dao/CoursesDao.java
+++ src/main/java/com/kalachev/task7/dao/CoursesDao.java
@@ -2,11 +2,8 @@ package com.kalachev.task7.dao;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
 import com.kalachev.task7.dao.entities.Course;
 
-@Component
 public interface CoursesDao {
 
   boolean addStudent(int studentId, String course);
--- src/main/java/com/kalachev/task7/dao/GroupsDao.java
+++ src/main/java/com/kalachev/task7/dao/GroupsDao.java
@@ -2,11 +2,8 @@ package com.kalachev.task7.dao;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
 import com.kalachev.task7.dao.entities.Group;
 
-@Component
 public interface GroupsDao {
   List<Group> findBySize(int size);
 }
