1a07066010bb04d32da3c86125f6afd0c7e75071
aoshaaa <kalachev.artemm@gmail.com>
2022-06-15 15:06:35 +0300

    fix

--- src/test/java/com/kalachev/task7/dao/DbUnitConfig.java
+++ src/test/java/com/kalachev/task7/dao/DbUnitConfig.java
@@ -1,8 +1,5 @@
 package com.kalachev.task7.dao;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -15,7 +12,10 @@ import org.dbunit.operation.DatabaseOperation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -23,13 +23,19 @@ import com.kalachev.task7.configuration.ConsoleAppConfig;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = ConsoleAppConfig.class)
+@PropertySource("classpath:DbProperties")
+@Component
 public class DbUnitConfig extends DBTestCase {
 
   Properties properties;
   IDatabaseTester databaseTester;
+  @Value("${JDBC_DRIVER}")
   String driver;
+  @Value("${URL}")
   String urlString;
+  @Value("${NAME}")
   String username;
+  @Value("${PASSWORD}")
   String password;
   IDataSet beforeData;
   @Autowired
@@ -37,34 +43,17 @@ public class DbUnitConfig extends DBTestCase {
   @Autowired
   JdbcTemplate template;
 
-  public DbUnitConfig() {
-    properties = new Properties();
-    URL url = ClassLoader.getSystemResource("DbProperties");
-    if (url != null) {
-      try (InputStream is = url.openStream()) {
-        properties.load(is);
-        this.driver = (String) properties.get("JDBC_DRIVER");
-        this.urlString = (String) properties.get("URL");
-        this.username = (String) properties.get("NAME");
-        this.password = (String) properties.get("PASSWORD");
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS,
-          driver);
-      System.setProperty(
-          PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, urlString);
-      System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME,
-          username);
-      System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD,
-          password);
-
-    }
-  }
-
   @Override
   @BeforeEach
   public void setUp() throws Exception {
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS,
+        driver);
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
+        urlString);
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME,
+        username);
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD,
+        password);
     databaseTester = new JdbcDatabaseTester(driver, urlString, username,
         password);
   }
